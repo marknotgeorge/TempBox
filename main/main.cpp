a91b5532ccb40ad119154c76772e7c63e2f9ae2c
Mark Johnson <marknotgeorge@googlemail.com>
2020-09-12 16:45:31 +0100

    Convert to C++ - Add wifi manager

---
+++ main/main.cpp
@@ -0,0 +1,68 @@
+/* Hello World Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include <string.h>
+#include <esp_wifi.h>
+#include <esp_netif.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include <esp_log.h>
+
+#include "wifi_manager.h"
+
+extern "C"
+{
+    void app_main();
+    void monitoring_task(void *pvParameter);
+    void cb_connection_ok(void *pvParameter);
+}
+
+static const char TAG[] = "main";
+
+/**
+ * @brief RTOS task that periodically prints the heap memory available.
+ * @note Pure debug information, should not be ever started on production code! This is an example on how you can integrate your code with wifi-manager
+ */
+void monitoring_task(void *pvParameter)
+{
+	for(;;){
+		ESP_LOGI(TAG, "free heap: %d",esp_get_free_heap_size());
+		vTaskDelay( pdMS_TO_TICKS(10000) );
+	}
+}
+
+
+/**
+ * @brief this is an exemple of a callback that you can setup in your own app to get notified of wifi manager event.
+ */
+void cb_connection_ok(void *pvParameter){
+	ip_event_got_ip_t* param = (ip_event_got_ip_t*)pvParameter;
+
+	/* transform IP to human readable string */
+	char str_ip[16];
+	esp_ip4addr_ntoa(&param->ip_info.ip, str_ip, IP4ADDR_STRLEN_MAX);
+
+	ESP_LOGI(TAG, "I have a connection and my IP is %s!", str_ip);
+}
+
+void app_main()
+{
+	/* start the wifi manager */
+	wifi_manager_start();
+
+	/* register a callback as an example to how you can integrate your code with the wifi manager */
+	wifi_manager_set_callback(WM_EVENT_STA_GOT_IP, &cb_connection_ok);
+
+	/* your code should go here. Here we simply create a task on core 2 that monitors free heap memory */
+	xTaskCreatePinnedToCore(&monitoring_task, "monitoring_task", 2048, NULL, 1, NULL, 1);
+}
+
+
